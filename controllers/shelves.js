d116c235ed21434677eb49e80acf16b03f0aaf3b
Jasmine Guy <jasfromnz@gmail.com>
2021-04-21 23:52:59 -0500

    wip: adds styling and sort by status on shelves page

--- controllers/shelves.js
+++ controllers/shelves.js
@@ -10,12 +10,18 @@ module.exports = {
 
 function index(req, res) {
     Shelf.find({}, (err, shelves) => {
+        const toRead = [], read = [], reading = [];
         shelves.forEach(function(book) {
             if (book.status === "toRead") {
                 book.status = "To Read";
-            }
+                toRead.push(book);
+            } else if (book.status === "read") {
+                read.push(book);
+            } else if (book.status === "reading") {
+                reading.push(book);
+            };
         });
-        res.render('index', { shelves });
+        res.render('index', { toRead, read, reading });
     });
 }
 
